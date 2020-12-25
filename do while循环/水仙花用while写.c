462b76a1b68c8ff342547e97bbdac61ab967d4f2
史金伟 <1627961573@qq.com>
2020-12-25 11:41:28 +0800

    do while循环练习

---
+++ do while循环/水仙花用while写.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main0000()
+{
+	int i = 100;
+	int a = i / 100;
+	int b = i % 10;
+	int c = (i % 100) / 10;
+	while (i <= 999)
+	{
+		if (a * a * a + b * b * b + c * c * c == i)
+		{
+			printf("Ë®ÏÉ»¨Êı£º%d\n", i);
+		}
+		i++;
+	}
+	return 0;
+
+}
---
+++ do while循环/用do while求100以内奇数.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main77()
+{
+	int i = 1;
+	do
+	{
+		if (i % 2 == 1)
+		{
+			printf("%d\n", i);
+		}
+		i++;
+	} while (i <= 999);
+		return 0;
+}
