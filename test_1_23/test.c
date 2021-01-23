23fcc8959357a644832557642eb0e64ce5ae2beb
史金伟 <1627961573@qq.com>
2021-01-23 21:21:43 +0800

    test_1_23

---
+++ test_1_23/test.c
@@ -0,0 +1,27 @@
+//1.½«×Ö·û´®½øĞĞÄæÖÃ
+//#include<stdio.h>
+//#include<string.h>
+//void NiZhi(char* str)
+//{
+//	int right = strlen(str);
+//	int left = 0;
+//	char* letf = str;
+//	char* righty = str + right;
+//	while (left < right)
+//	{
+//		char temp =*letf;
+//		*letf = *righty;
+//		*righty = temp;
+//		letf++;
+//		righty--;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char str[] = "hello bit";
+//	NiZhi(str);
+//	printf("%s", str);
+//	return 0;
+//}
