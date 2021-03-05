0b4397d6b7eaf8cc58b0faa0452642ede294754f
史金伟 <1627961573@qq.com>
2021-03-05 21:48:39 +0800

    test_3_5

---
+++ test_3_5/源.c
@@ -0,0 +1,59 @@
+//#include<stdio.h>
+//int main()
+//{
+//	printf("This is a C program\n");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, sum;
+//	a = 123, b = 456;
+//	sum = a + b;
+//	printf("sum is %d", sum);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int max(int x, int y);
+//	int a, b, c;
+//	printf("input a&b:");
+//	scanf_s("%d%d", &a, &b);
+//	c = max(a, b);
+//	printf("max=%d\n", c);
+//	return 0;
+//}
+//int max(int x, int y)
+//{
+//	int z;
+//	if (x > y)
+//	{
+//		z = x;
+//	}
+//	else
+//	{
+//		z = y;
+//	}
+//	return z;
+//}
+
+
+//求三者之中最大值
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c, max;
+//	printf("input a&b&c:");
+//	scanf_s("%d%d%d", &a, &b, &c);
+//	max = a > b ? a : b;
+//	max = max > c ? max : c;
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+
+
