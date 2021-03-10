222306d83a96aac6610498e745d9e84fd351fa8a
史金伟 <1627961573@qq.com>
2021-03-10 21:33:22 +0800

    test_3_10

---
+++ test_3_10/源.c
@@ -0,0 +1,17 @@
+//自定义类型
+//结构体内存对齐：结构体在计算大小时会存在内存对齐
+//vs中默认最大对齐数为8
+//默认对齐数的修改：#program pack(自定义最大对齐数)
+//恢复默认对齐数：#program pack();
+#include<stdio.h>
+struct stu
+{
+	char c1;
+	int i;
+	char c2;
+}S;
+int main()
+{
+	printf("%d",sizeof(S));//12字节
+}
+
