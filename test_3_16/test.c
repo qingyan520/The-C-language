b575643d438c813f387703b0ea067fc1f98e03a3
史金伟 <1627961573@qq.com>
2021-03-16 22:52:51 +0800

    test_3_16

---
+++ test_3_16/test.c
@@ -0,0 +1,12 @@
+//±‡“Î∫Õ‘§¥¶¿Ì
+#include<stdio.h>
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d %s %d\n", __LINE__, __FILE__, i);
+	}
+	system("pause");
+	return 0;
+	return 0;
+}
