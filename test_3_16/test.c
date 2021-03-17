14202ca755519482ce5d41ecbf7157c0311867f5
史金伟 <1627961573@qq.com>
2021-03-17 20:22:13 +0800

    test_3_17

--- test_3_16/test.c
+++ test_3_16/test.c
@@ -1,12 +1,14 @@
 //编译和预处理
-#include<stdio.h>
-int main()
-{
-	for (int i = 0; i < 10; i++)
-	{
-		printf("%d %s %d\n", __LINE__, __FILE__, i);
-	}
-	system("pause");
-	return 0;
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d %s %d\n", __LINE__, __FILE__, i);
+//	}
+//	system("pause");
+//	return 0;
+//	return 0;
+//}
+//#define :1.定义符号，标识符：#define MAX  100
+//         2.定义宏:#define _CRT_SCURE_NO_WARINGS
