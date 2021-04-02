72de4442caf6c25fac8d45c0e8b9041336cd1a51
史金伟 <1627961573@qq.com>
2021-04-02 20:45:00 +0800

    test_4_2

---
+++ test_4_2/test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int c1,c2;
+	printf("ÇëÊäÈë×Ö·ûc1,c2:\n");
+	c1=getchar();
+	c2=getchar();
+	putchar(c1);
+	putchar(c2);
+	printf("\n");
+	printf("c1=%d\n", c1);
+	printf("c2=%d\n", c2);
+	printf("c1=%c\n", c1);
+	printf("c2=%c\n", c2);
+	return 0;
+}
