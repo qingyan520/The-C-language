c7eef886e3711245658b5989abb74be8860a50cb
史金伟 <1627961573@qq.com>
2021-04-09 21:00:37 +0800

    test_4_9

---
+++ test_4_9/test.c
@@ -0,0 +1,64 @@
+//#include<st:dio.h>
+//int main()
+//{
+//	int x = 0;
+//	while (scanf_s("%d", &x) != EOF)
+//	{
+//		if (x < 1)
+//		{
+//			printf("%d\n", x);
+//		}
+//		else if (x >= 1 && x < 10)
+//		{
+//			printf("%d\n", 2 * x - 1);
+//		}
+//		else
+//		{
+//			printf("%d\n", 3 * x - 11);
+//		}
+//	}
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	int x = 0;
+//	scanf_s("%d", &x);
+//	
+//	double y = sqrt(x);	
+//	printf("%lf\n",y);
+//	
+//	return 0;
+//}
+
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int score = 0;
+//	scanf_s("%d", &score);
+//	switch (score / 10)
+//	{
+//	case 10:
+//	case 9:
+//		printf("A");
+//		break;
+//	case 8:
+//		printf("B");
+//		break;
+//	case 7:
+//		printf("C");
+//		break;
+//	case 6:
+//		printf("D");
+//		break;
+//	default:
+//		printf("E");
+//		break;
+//	}
+//	return 0;
+//}
+
+
