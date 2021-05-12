2c0029818be404b179eb33980dd34ac0b2dd15ff
史金伟 <1627961573@qq.com>
2021-05-12 21:20:26 +0800

    排序算法合集

--- 排序算法合集/4.归并排序.c
+++ 排序算法合集/4.归并排序.c
@@ -6,70 +6,70 @@
 //1  2  3  4  6  7  9  10
 //取小的数据尾插到下面数组，知道一个区间结束，再把另一个区间剩下元素尾插到最后
 
-#include<stdio.h>
-#include<stdlib.h>
-void _MergeSort(int* arr, int left, int right, int* temp)
-{
-	if (left >= right)
-	{
-		return;
-	}
-	int mid = (right + left) / 2;
-	_MergeSort(arr, left, mid, temp);
-	_MergeSort(arr, mid + 1, right, temp);
-	int begin1 = left;
-	int end1 = mid;
-	int begin2 = mid + 1;
-	int end2 = right;
-	int i = left;
-	while (begin1 <= end1 && begin2 <= end2)
-	{
-		if (arr[begin1] < arr[begin2])
-		{
-			temp[i++] = arr[begin1++];
-		}
-		else
-		{
-			temp[i++] = arr[begin2++];
-		}
-	}
-	while (begin1 <= end1)
-	{
-		temp[i++] = arr[begin1++];
-	}
-	while(begin2 <= end2)
-	{
-		temp[i++] = arr[begin2++];
-	}
-	for (int i = left; i <= right; i++)
-	{
-		arr[i] = temp[i];
-	}
-
-
-}
-void MergeSort(int* arr, int n)
-{
-	int* temp = malloc(sizeof(int) * n);
-	if (temp == NULL)
-	{
-		printf("malloc fail\n");
-		exit(-1);
-	}
-	_MergeSort(arr, 0, n - 1, temp);
-	free(temp);
-}
-int main()
-{
-	int arr[] = { 2,1,5,4,3,6,8,9,7,10 };
-	int len = sizeof(arr) / sizeof(arr[0]);
-	MergeSort(arr, len);
-	for (int i = 0; i < len; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//void _MergeSort(int* arr, int left, int right, int* temp)
+//{
+//	if (left >= right)
+//	{
+//		return;
+//	}
+//	int mid = (right + left) / 2;
+//	_MergeSort(arr, left, mid, temp);
+//	_MergeSort(arr, mid + 1, right, temp);
+//	int begin1 = left;
+//	int end1 = mid;
+//	int begin2 = mid + 1;
+//	int end2 = right;
+//	int i = left;
+//	while (begin1 <= end1 && begin2 <= end2)
+//	{
+//		if (arr[begin1] < arr[begin2])
+//		{
+//			temp[i++] = arr[begin1++];
+//		}
+//		else
+//		{
+//			temp[i++] = arr[begin2++];
+//		}
+//	}
+//	while (begin1 <= end1)
+//	{
+//		temp[i++] = arr[begin1++];
+//	}
+//	while(begin2 <= end2)
+//	{
+//		temp[i++] = arr[begin2++];
+//	}
+//	for (int i = left; i <= right; i++)
+//	{
+//		arr[i] = temp[i];
+//	}
+//
+//
+//}
+//void MergeSort(int* arr, int n)
+//{
+//	int* temp = malloc(sizeof(int) * n);
+//	if (temp == NULL)
+//	{
+//		printf("malloc fail\n");
+//		exit(-1);
+//	}
+//	_MergeSort(arr, 0, n - 1, temp);
+//	free(temp);
+//}
+//int main()
+//{
+//	int arr[] = { 2,1,5,4,3,6,8,9,7,10 };
+//	int len = sizeof(arr) / sizeof(arr[0]);
+//	MergeSort(arr, len);
+//	for (int i = 0; i < len; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
 
 //#include<stdio.h>
 //
---
+++ 排序算法合集/快排的非递归版本.cpp
@@ -0,0 +1,75 @@
+//利用栈来模拟实现快排的非递归实现
+//我们先让左右区间入栈
+//例如：
+//0             9
+//则出栈顺序为：9（right）     0（left）
+//然后找到keyi1下标位置 ,然后让这三个元素分别入栈
+//0     keyi1         9
+//再出栈  9（right）    keyi1(left)
+//再找到这两段位置的中间keyi，这时就会循环在右边的区间中使每一个数到合适的位置
+//一直循环直到不能再插入元素，然后出栈，循环左边的序列
+
+
+#include<stdio.h>
+#include<stack>
+using namespace std;
+void Swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int PastSort(int* arr, int left, int right)
+{
+	int keyi = left;
+	while (left < right)
+	{
+		while (left < right && arr[right] >= arr[keyi])
+		{
+			right--;
+		}
+		while (left < right && arr[left] <= arr[keyi])
+		{
+			left++;
+		}
+		Swap(&arr[left], &arr[right]);
+	}
+	Swap(&arr[keyi], &arr[left]);
+	return left;
+}
+void QuickSort(int* arr, int begin,int end)
+{
+	stack<int> st;
+	st.push(begin);
+	st.push(end);
+	while (!st.empty())
+	{
+		int left;
+		int right;
+		right = st.top();
+		st.pop();
+		left = st.top();
+		st.pop();
+		int keyi=PastSort(arr, left, right);
+		if (left < keyi - 1)
+		{
+			st.push(left);
+			st.push(keyi - 1);
+		}
+		if (right > keyi + 1)
+		{
+			st.push(keyi + 1);
+			st.push(right);
+		}
+	}
+
+}
+int main()
+{
+	int arr[] = { 7,10,2,4,5,3,6,9,8,1 };
+	QuickSort(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
